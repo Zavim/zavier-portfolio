8807cf32fa7e2468831dd9dcde5c2121b67ba123
Zavim <Zark471@gmail.com>
2023-05-30 16:30:44 -0400

    -f use instancing on stars

--- src/components/FloatingStars.jsx
+++ src/components/FloatingStars.jsx
@@ -1,20 +1,88 @@
 import * as THREE from "three";
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useGLTF, Environment } from "@react-three/drei";
+import { useGLTF, Environment, Instances, Instance } from "@react-three/drei";
 import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
-// import { Perf } from "r3f-perf";
+import { Perf } from "r3f-perf";
 
-function Star({ z, animate }) {
-  const starRef = useRef();
+// function Star({ z, animate }) {
+//   const starRef = useRef();
+//   const { nodes, materials } = useGLTF("/kirbyStar-transformed.glb");
+//   const { viewport, camera } = useThree();
+//   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+
+//   const [data] = useState({
+//     x: THREE.MathUtils.randFloatSpread(2), //this creates a range between -+x/2
+//     y: THREE.MathUtils.randFloatSpread(height),
+//     rX: Math.random() * Math.PI,
+//     rY: Math.random() * Math.PI,
+//     rZ: Math.random() * Math.PI,
+//   });
+
+//   useFrame((_state, delta) => {
+//     if (animate) {
+//       starRef.current.rotation.set(
+//         (data.rX += delta / 2),
+//         (data.rY += delta / 2),
+//         (data.rZ += delta / 2)
+//       );
+//       starRef.current.position.set(data.x * width, (data.y += delta / 3), z);
+//       if (data.y > height) data.y = -height;
+//     }
+//   });
+
+//   return (
+//     <mesh
+//       ref={starRef}
+//       geometry={nodes.Roundcube.geometry}
+//       material={materials.star_material}
+//       material-color="yellow"
+//       material-emissive="orange"
+//     />
+//   );
+// }
+
+function Stars({ count, depth, animate }) {
   const { nodes, materials } = useGLTF("/kirbyStar-transformed.glb");
   const { viewport, camera } = useThree();
-  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+  let data = [];
+  for (let i = 0; i < count; i++) {
+    const z = (-i / count) * depth;
+    const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
+    data.push({
+      random: Math.random(),
+      position: [0, 0, z],
+      width: width,
+      height: height,
+      z: z,
+    });
+  }
+  return (
+    <Instances
+      material={materials.star_material}
+      material-color="yellow"
+      material-emissive="orange"
+      geometry={nodes.Roundcube.geometry}
+    >
+      <group position={[0, 0, 0]}>
+        {data.map((props, i) => (
+          <Star key={i} animate={animate} {...props} />
+        ))}
+      </group>
+    </Instances>
+  );
+}
+
+function Star({ random, animate, ...props }) {
+  const height = props.height;
+  const width = props.width;
+  const z = props.z;
+  const starRef = useRef();
+  const rotationScalar = 5;
 
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2), //this creates a range between -+x/2
-    // y: THREE.MathUtils.randFloatSpread(height),
-    y: height,
+    y: THREE.MathUtils.randFloatSpread(height),
     rX: Math.random() * Math.PI,
     rY: Math.random() * Math.PI,
     rZ: Math.random() * Math.PI,
@@ -22,10 +90,11 @@ function Star({ z, animate }) {
 
   useFrame((_state, delta) => {
     if (animate) {
+      starRef.current.position.set(data.x, data.y, z);
       starRef.current.rotation.set(
-        (data.rX += delta / 2),
-        (data.rY += delta / 2),
-        (data.rZ += delta / 2)
+        (data.rX += delta / rotationScalar),
+        (data.rY += delta / rotationScalar),
+        (data.rZ += delta / rotationScalar)
       );
       starRef.current.position.set(data.x * width, (data.y += delta / 3), z);
       if (data.y > height) data.y = -height;
@@ -33,19 +102,14 @@ function Star({ z, animate }) {
   });
 
   return (
-    <mesh
-      ref={starRef}
-      geometry={nodes.Roundcube.geometry}
-      material={materials.star_material}
-      material-color="yellow"
-      material-emissive="orange"
-    />
+    <group {...props}>
+      <Instance ref={starRef} />
+    </group>
   );
 }
 
-export default function FloatingStars({ count = 20, depth = 30 }) {
+export default function FloatingStars({ count = 50, depth = 30 }) {
   const [animate, setAnimate] = useState(true);
-
   const toggleAnimation = (animate) => {
     setAnimate(!animate);
   };
@@ -71,35 +135,36 @@ export default function FloatingStars({ count = 20, depth = 30 }) {
         </div>
       </button>
 
-      {/* <Suspense fallback={<div className="loading"></div>}> */}
       <Canvas
         style={{ width: "100vw", height: "100vh" }}
         gl={{ alpha: false }}
         camera={{ near: 0.01, far: 110, fov: 40 }}
       >
-        {/* <Perf /> */}
         <color attach="background" args={["#fe9bcb"]} />
+        <Perf />
         <spotLight position={[10, 10, 10]} intensity={1} />
-        <Environment preset="sunset" />
-        {Array.from({ length: count }, (_, i) => (
-          <Star key={i} z={(-i / count) * depth} animate={animate} />
-        ))}
-        <EffectComposer>
-          <DepthOfField
-            focusDistance={1.5}
-            focalLength={1}
-            bokehScale={2}
-            // height={700}
-          />
-          {/* <DepthOfField
-            target={[0, 0, depth / 4]}
-            focalLength={1}
-            bokehScale={2}
-            height={700}
-          /> */}
-        </EffectComposer>
+        <Suspense fallback={null}>
+          <Environment preset="sunset" />
+          {/* {Array.from({ length: count }, (_, i) => (
+            <Star key={i} z={(-i / count) * depth} animate={animate} />
+          ))} */}
+          <Stars count={count} depth={depth} animate={animate} />
+          <EffectComposer>
+            {/* <DepthOfField
+              focusDistance={1.5}
+              focalLength={1}
+              bokehScale={2}
+              // height={700}
+            /> */}
+            <DepthOfField
+              target={[0, 0, depth / 2]}
+              focalLength={0.1}
+              bokehScale={2}
+              height={700}
+            />
+          </EffectComposer>
+        </Suspense>
       </Canvas>
-      {/* </Suspense> */}
     </>
   );
 }
